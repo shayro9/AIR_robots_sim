97956a54a8045c98674d276bfe2fe1eab18e9dcf
Shiran Peeran <85098219+shiran96@users.noreply.github.com>
2024-12-31 11:37:17 +0200

    Add files via upload

---
+++ sim_ur5/mujoco_env/__init__.py
@@ -0,0 +1,26 @@
+# from . import milestones  # registers the milestones
+from gymnasium import make
+from .common import cfg_keys
+from .episode import *
+from .mujoco_env import MujocoEnv
+from .tasks.rearrangement.rearrangement_task import COMRearrangementTask as _COMTask
+from .tasks.null_task import NullTask
+
+__all__ = [
+    'make',
+    'MujocoEnv',
+    'from_cfg',
+    'from_cfg_file',
+    'cfg_keys',
+    'EpisodeSpec',
+    'SceneSpec',
+    'RobotSpec',
+    'TaskSpec',
+    'ObjectSpec',
+    'AttachmentSpec',
+    'MountSpec',
+    'NullTask',
+]
+
+from_cfg = MujocoEnv.from_cfg
+from_cfg_file = MujocoEnv.from_cfg_file
